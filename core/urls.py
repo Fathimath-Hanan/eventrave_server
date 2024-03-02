e4fa078f40d7434b8c59ad1232780aaedc66b85b
Fathimath Hanan <F20gcs04@meaec.edu.in>
2024-03-02 21:17:36 +0530

    #1done again

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from core.views import CertificateDetailAPIView, CertificateListAPIView, EventDetailAPIView, EventListAPIView, JudgeDetailAPIView, JudgeListAPIView, ParticipantDetailAPIView, ParticipantListAPIView, ScoreDetailAPIView, ScoreListAPIView
+from core.views import CertificateDetailAPIView, CertificateListAPIView, EventDetailAPIView, EventListAPIView, JudgeDetailAPIView, JudgeListAPIView, ParticipantDetailAPIView, ParticipantListAPIView, ScoreDetailAPIView, ScoreListAPIView, StudentHomeAPIView
 
 urlpatterns = [
     path('event/',EventListAPIView.as_view()),
@@ -16,4 +16,5 @@ urlpatterns = [
     path('score/<int:pk>/',ScoreDetailAPIView.as_view()),
     path('judge/',JudgeListAPIView.as_view()),
     path('judge/<int:pk>/',JudgeDetailAPIView.as_view()),
+    path('studenthome/',StudentHomeAPIView.as_view())
 ]
