6b4b74730c39ef01005c7bd35ba73c7b59d3af4b
Fathimath Hanan <156740030+Fathimath-Hanan@users.noreply.github.com>
2024-03-11 09:22:52 +0530

    #8judge home done

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from core.views import CertificateDetailAPIView, CertificateListAPIView, EventDetailAPIView, EventListAPIView, JudgeDetailAPIView, JudgeListAPIView, ParticipantDetailAPIView, ParticipantListAPIView, ScoreDetailAPIView, ScoreListAPIView, StudentHomeAPIView
+from core.views import CertificateDetailAPIView, CertificateListAPIView, EventDetailAPIView, EventListAPIView, JudgeDetailAPIView, JudgeHomeAPIView, JudgeListAPIView, ParticipantDetailAPIView, ParticipantListAPIView, ScoreDetailAPIView, ScoreListAPIView, StudentHomeAPIView
 
 urlpatterns = [
     path('event/',EventListAPIView.as_view()),
@@ -16,5 +16,6 @@ urlpatterns = [
     path('score/<int:pk>/',ScoreDetailAPIView.as_view()),
     path('judge/',JudgeListAPIView.as_view()),
     path('judge/<int:pk>/',JudgeDetailAPIView.as_view()),
-    path('studenthome/',StudentHomeAPIView.as_view())
+    path('studenthome/',StudentHomeAPIView.as_view()),
+    path('judgehome/',JudgeHomeAPIView.as_view())
 ]
