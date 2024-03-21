85fb11908da64733f390f0b1a31b695708f89979
Fathimath Hanan <F20gcs04@meaec.edu.in>
2024-03-21 23:58:12 +0530

    ysss

--- core/api/judge/urls.py
+++ core/api/judge/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from core.api.judge.judge_event_participant import RegisteredParticipantsAPIView
 
 from core.api.judge.judge_home_events import JudgeHomeAPIView
 
@@ -9,4 +10,5 @@ from core.api.judge.judge_home_events import JudgeHomeAPIView
 
 urlpatterns = [
 path('homeevents/',JudgeHomeAPIView.as_view()),
+path('participantlist/<int:event_id>/',RegisteredParticipantsAPIView.as_view()),
 ]
